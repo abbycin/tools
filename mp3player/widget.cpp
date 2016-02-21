8689d6099f908afc0726bd12f3cd514abdfa3aa2
abbycin <abbytsing@gmail.com>
2016-02-21 14:59:15 +0800

    improve: modified interface

--- mp3player/widget.cpp
+++ mp3player/widget.cpp
@@ -23,20 +23,29 @@ Widget::~Widget()
 
 int Widget::load()
 {
-    QString path = music_path;
-    if(path[path.size() - 1] != '/')
-        path.append('/');
-    QDir d(path);
+    QDir d(music_path);
+    music_path = d.absolutePath();
     if(!d.exists())
+    {
+        ui->song_label->setText(music_path + " not exsits!");
         return -1;
+    }
     QStringList filters;
     filters << "*.mp3";
     QStringList items = d.entryList(filters);
+    if(items.size() == 0)
+    {
+        ui->song_label->setText("no mp3 file in " + music_path);
+        return -1;
+    }
     QList<QMediaContent> contents;
+
+    if(music_path[music_path.size() - 1] != '/')
+        music_path.append('/');
 
     for(auto &x: items)
     {
-        contents.push_back(QUrl::fromLocalFile(path + x));
+        contents.push_back(QUrl::fromLocalFile(music_path + x));
     }
 
     list.addMedia(contents);
@@ -55,7 +64,6 @@ void Widget::setup(const QString &path)
     music_path = path;
     if(load() < 0)
     {
-        ui->song_label->setText(music_path + " not exsits!");
         everything_ok = false;
     }
     else
@@ -63,7 +71,8 @@ void Widget::setup(const QString &path)
         list.setPlaybackMode(QMediaPlaylist::Loop);
         player->setPlaylist(&list);
         player->setVolume(50);
-        ui->song_label->setText(path);
+        ui->start->setText("play");
+        ui->song_label->setText(music_path);
         ui->volume_bar->setValue(player->volume());
         ui->volume_label->setText(QString::number(player->volume()));
     }
@@ -73,19 +82,16 @@ void Widget::on_start_clicked()
 {
     if(!everything_ok)
         return;
-    QString display;
     if(player->state() == QMediaPlayer::PlayingState)
     {
-        display = "Paused: ";
-        display += current_playing();
-        ui->song_label->setText(display);
+        ui->start->setText("play");
+        ui->song_label->setText(current_playing());
         player->pause();
     }
     else
     {
-        display = "Playing: ";
-        display += current_playing();
-        ui->song_label->setText(display);
+        ui->start->setText("pause");
+        ui->song_label->setText(current_playing());
         player->play();
     }
 }
@@ -128,8 +134,9 @@ void Widget::on_volume_bar_valueChanged(int value)
 void Widget::on_currentMediaChanged(QMediaContent media)
 {
     auto x = media.canonicalUrl().path().split('/');
-    QString s = "Playing: ";
+    QString s;
     if(x.size() > 0)
-        s += x[x.size() - 1];
+        s = x[x.size() - 1];
     ui->song_label->setText(s);
+    ui->song_label->setToolTip(s);
 }
