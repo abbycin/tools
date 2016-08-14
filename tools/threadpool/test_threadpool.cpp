85a170593b60b6d0b2632ccc4c535d87c6a9b6d3
abbycin <abbytsing@gmail.com>
2016-08-14 18:58:31 +0800

    fix: invalid function object bug.

--- tools/threadpool/test_threadpool.cpp
+++ tools/threadpool/test_threadpool.cpp
@@ -24,6 +24,7 @@ int main()
   pool.add_task(f, 1);
   pool.add_task(f, 2);
   pool.add_task(f, 3);
+  pool.pause();
   pool.add_task(f, 4);
   pool.add_task(f, 5);
   pool.add_task(f, 6);
@@ -41,8 +42,36 @@ int main()
   {
     cout << e.what() << endl;
   }
+  pool.start();
+  pool.wait();
   this_thread::sleep_for(chrono::seconds(1));
   pool.stop();
   if(pool.valid())
     cout << "get: " << fut.get() << endl;
+  this_thread::sleep_for(chrono::seconds(3));
+  cout << "--------------------------------------------------------\n";
+  auto f2 = [](int x)
+  {
+    cout << "tid: " << this_thread::get_id() << "\tdata: " << (x * 2) << endl;
+  };
+  nm::threadpool pool2(std::launch::deferred);
+  pool2.add_task(f2, -1);
+  pool2.add_task(f2, -2);
+  pool2.add_task(f2, -3);
+  pool2.add_task(f2, -4);
+  pool2.start();
+  pool2.wait();
+  pool2.stop();
+  try
+  {
+    pool2.add_task(f2, -4);
+  }
+  catch(runtime_error& e)
+  {
+    cout << "Exception: " << e.what() << endl;
+  }
+  nm::threadpool pool3(std::launch::deferred);
+  pool3.pause();  // set `is_start_` flag in destructor
+  pool3.wait();
+  cout << "done.\n";
 }
--- tools/threadpool/threadpool.h
+++ tools/threadpool/threadpool.h
@@ -25,7 +25,9 @@ namespace nm
   class threadpool final
   {
     public:
-      threadpool(std::size_t threads = std::thread::hardware_concurrency());
+      threadpool();
+      threadpool(std::launch, 
+          std::size_t threads = std::thread::hardware_concurrency());
       threadpool(threadpool&&) = delete;
       threadpool(const threadpool&) = delete;
       threadpool& operator=(const threadpool&) = delete;
@@ -35,77 +37,92 @@ namespace nm
       -> std::future<typename std::result_of<F(Args...)>::type>;
       size_t queue_size_limit();
       void set_queue_size_limit(size_t);
+      void start();
+      void wait();
+      void pause();
       void stop();
-      bool valid();
+      bool valid() const;
     private:
+      std::launch policy_;
       // we don't use `atomic` variable here.
-      bool is_exit;
-      bool is_stop;
-      size_t TASK_LIMIT;
-      std::vector<std::thread> workers;
-      std::queue<std::function<void()>> tasks;
-      std::mutex queue_lock;
-      std::condition_variable queue_cond;
+      bool is_exit_;
+      bool is_stop_;
+      bool is_start_;
+      size_t task_limit_;
+      std::vector<std::thread> workers_;
+      std::queue<std::function<void()>> tasks_;
+      mutable std::mutex queue_lock_;
+      std::condition_variable queue_cond_, wait_cond_;
+      void check_status_(const std::string& func)
+      {
+        if(is_stop_)
+          throw std::runtime_error(func + " on stopped threadpool.");
+      }
   };
-  threadpool::threadpool(size_t threads)
-    : is_exit(false), is_stop(false), TASK_LIMIT(1000)
+  threadpool::threadpool(std::launch p, size_t threads)
+    : policy_(p), is_exit_(false), is_stop_(false),
+    is_start_(policy_ == std::launch::async ? true : false), task_limit_(1000)
   {
     for(;threads > 0; --threads)
     {
-      workers.emplace_back([&]
+      workers_.emplace_back([&]
         {
-          std::function<void()> task;
           for(;;)
           {
-            {
-              std::unique_lock<std::mutex> l(queue_lock);
-              queue_cond.wait(l, [this]
-                {
-                  return is_stop || is_exit || !tasks.empty();
-                }
-              );
-              // caller request to stop all task.
-              if(is_stop)
-                return;
-              // normal exit, when pool is going to destroyed.
-              if(tasks.empty())
+            std::unique_lock<std::mutex> l(queue_lock_);
+            queue_cond_.wait(l, [this]
               {
-                if(is_exit)
-                  break;
+                if(!is_start_)
+                  return false;
+                return is_stop_ || is_exit_ || !tasks_.empty();
               }
-              else
+            );
+            // caller request to stop all task.
+            if(is_stop_)
+              return;
+            // normal exit, when pool is going to destroyed.
+            if(tasks_.empty())
+            {
+              if(is_exit_)
+                break;
+            }
+            else
+            {
+              auto task(std::move(tasks_.front()));
+              tasks_.pop();
+              if(tasks_.empty())
+                wait_cond_.notify_one();
+              l.unlock();
+              try
               {
-                task = move(tasks.front());
-                tasks.pop();
+                task();
               }
-            }
-            try
-            {
-              task();
-            }
-            catch(std::exception& e)
-            {
-              // abort program when encounter an exception.
-              assert(false);
+              catch(std::exception& e)
+              {
+                // abort program when encounter an exception.
+                assert(false);
+              }
             }
           }
         }
       );
     }
   }
+  threadpool::threadpool() : threadpool(std::launch::async) {}
   threadpool::~threadpool() noexcept
   {
     try
     {
-      std::unique_lock<std::mutex> l(queue_lock);
-      is_exit = true;
-      queue_cond.notify_all();
+      std::unique_lock<std::mutex> l(queue_lock_);
+      is_start_ = true;
+      is_exit_ = true;
+      queue_cond_.notify_all();
     }
     catch(std::system_error&)
     {
       assert(false);
     }
-    for(auto& t: workers)
+    for(auto& t: workers_)
     {
       try
       {
@@ -125,13 +142,13 @@ namespace nm
     auto task = std::make_shared<std::packaged_task<R()>>(
         std::bind(std::forward<F>(f), std::forward<Args>(args)...));
     auto res(task->get_future());
-    std::unique_lock<std::mutex> l(queue_lock);
-    if(tasks.size() >= TASK_LIMIT)
+    std::unique_lock<std::mutex> l(queue_lock_);
+    if(tasks_.size() >= task_limit_)
       throw std::runtime_error("task queue is full.");
-    if(is_stop)
-      throw std::runtime_error("add_task on stopped threadpool.");
-    tasks.emplace([task]{ (*task)(); });
-    queue_cond.notify_one();
+    check_status_(__func__);
+    tasks_.emplace([task]{ (*task)(); });
+    if(policy_ == std::launch::async)
+      queue_cond_.notify_one();
     // NRVO; gcc with -fno-elide-constructros will call move constructor
     // in 'Primary template' (gcc6.1: in header file future at line 749)
     // Thus, we don't use 'std::move' here.
@@ -139,39 +156,53 @@ namespace nm
   }
   std::size_t threadpool::queue_size_limit()
   {
-    std::size_t res;
-    {
-      std::unique_lock<std::mutex> l(queue_lock);
-      res = TASK_LIMIT;
-    }
-    return res;
+    std::unique_lock<std::mutex> l(queue_lock_);
+    check_status_(__func__);
+    return task_limit_;
   }
   void threadpool::set_queue_size_limit(std::size_t size)
   {
-    std::unique_lock<std::mutex> l(queue_lock);
-    auto old_limit = TASK_LIMIT;
-    TASK_LIMIT = (size > 1 ? size : 1);
-    // if new limit less then old one,then wake up all workers to
-    // process as many tasks as they can.
-    if(TASK_LIMIT < old_limit)
-      queue_cond.notify_all();
+    std::unique_lock<std::mutex> l(queue_lock_);
+    check_status_(__func__);
+    auto old_limit = task_limit_;
+    task_limit_ = (size > 1 ? size : 1);
+    // if new limit less then old one,then wake up all workers_ to
+    // process as many tasks_ as they can.
+    if(task_limit_ < old_limit)
+      queue_cond_.notify_all();
+  }
+  void threadpool::start()
+  {
+    std::unique_lock<std::mutex> l(queue_lock_);
+    check_status_(__func__);
+    is_start_ = true;
+    queue_cond_.notify_all();
+  }
+  void threadpool::wait()
+  {
+    std::unique_lock<std::mutex> l(queue_lock_);
+    check_status_(__func__);
+    wait_cond_.wait(l, [&] { return tasks_.empty(); });
+  }
+  void threadpool::pause()
+  {
+    std::unique_lock<std::mutex> l(queue_lock_);
+    check_status_(__func__);
+    is_start_ = false;
   }
   void threadpool::stop()
   {
-    std::unique_lock<std::mutex> l(queue_lock);
-    is_stop = true;
-    while(!tasks.empty())
-      tasks.pop();
-    queue_cond.notify_all();
+    std::unique_lock<std::mutex> l(queue_lock_);
+    check_status_(__func__);
+    is_stop_ = true;
+    while(!tasks_.empty())
+      tasks_.pop();
+    queue_cond_.notify_all();
   }
-  bool threadpool::valid()
+  bool threadpool::valid() const
   {
-    bool res;
-    {
-      std::unique_lock<std::mutex> l(queue_lock);
-      res = !tasks.empty();
-    }
-    return res;
+    std::unique_lock<std::mutex> l(queue_lock_);
+    return !is_stop_;
   }
 }
 
