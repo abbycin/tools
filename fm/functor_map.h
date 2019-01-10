0f8a74a8dac4d223508883fa0b17a8b44cd4b938
abbycin <abbytsing@gmail.com>
2019-01-10 20:22:01 +0800

    1. fix ub of functor_map 2. add return type and argument type constraint

---
+++ fm/functor_map.h
@@ -0,0 +1,188 @@
+/*********************************************************
+          File Name: functor_map.h
+          Author: Abby Cin
+          Mail: abbytsing@gmail.com
+          Created Time: Sat 07 Apr 2018 03:43:56 PM CST
+**********************************************************/
+
+#ifndef FUNCTOR_MAP_H_
+#define FUNCTOR_MAP_H_
+
+#include <functional>
+#include <list>
+#include <map>
+#include <mutex>
+#include <string>
+
+namespace nm
+{
+  namespace meta
+  {
+    template<typename...> struct test_arg;
+    template<> struct test_arg<>
+    {
+      constexpr static bool value = true;
+    };
+    template<typename T> struct test_arg<T>
+    {
+      constexpr static bool value = !std::is_reference_v<T> && std::is_copy_assignable_v<T> && std::is_constructible_v<T>;
+    };
+    template<typename T, typename... Ts> struct test_arg<T, Ts...>
+    {
+      constexpr static bool value = test_arg<T>::value && test_arg<Ts...>::value;
+    };
+
+    template<typename> struct Inspector;
+    template<typename R, typename... Args>
+    struct Inspector<R(Args...)>
+    {
+      constexpr static bool is_r_valid = std::is_void_v<R> || (std::is_copy_assignable_v<R> && std::is_constructible_v<R>);
+      constexpr static bool is_arg_valid = test_arg<Args...>::value;
+      static_assert(is_r_valid, "invalid return type, must be `void` or can be copied and constructed");
+      static_assert(is_arg_valid, "invalid argument type, must be copyable and can't be reference");
+      using arg_t = std::tuple<Args...>;
+      using res_t = R;
+    };
+    template<typename R, typename... Args>
+    struct Inspector<R(*)(Args...)> : Inspector<R(Args...)> {};
+    template<typename R, typename... Args>
+    struct Inspector<R(&)(Args...)> : Inspector<R(Args...)> {};
+    template<typename R, typename Object, typename... Args>
+    struct Inspector<R(Object::*)(Args...)> : Inspector<R(Args...)> {};
+    template<typename R, typename Object, typename... Args>
+    struct Inspector<R(Object::*)(Args...) const> : Inspector<R(Args...)> {};
+    template<typename R, typename Object, typename... Args>
+    struct Inspector<R(Object::*)(Args...) volatile> : Inspector<R(Args...)> {};
+    template<typename R, typename Object, typename... Args>
+    struct Inspector<R(Object::*)(Args...) const volatile> : Inspector<R(Args...)> {};
+
+    // functor like
+    template<typename Lambda>
+    struct Inspector : Inspector<decltype(&Lambda::operator())> {};
+    template<typename Lambda>
+    struct Inspector<Lambda&> : Inspector<decltype(&Lambda::operator())> {};
+    template<typename Lambda>
+    struct Inspector<Lambda&&> : Inspector<Lambda&> {};
+
+    template<typename R> struct call_impl
+    {
+      template<typename F, typename Arg>
+      static R call(F&& f, Arg& args)
+      {
+        R res;
+        std::forward<F>(f)(&args, &res);
+        return res;
+      }
+
+      template<typename F, typename TupleType, size_t... Is>
+      static void invoke(R* res, F&& f, TupleType& args, std::index_sequence<Is...>)
+      {
+        *res = std::forward<F>(f)(std::forward<std::tuple_element_t<Is, std::remove_reference_t<TupleType>>>(std::get<Is>(args))...);
+      }
+    };
+    template<> struct call_impl<void>
+    {
+      template<typename F, typename Arg>
+      static void call(F&& f, Arg& args)
+      {
+        std::forward<F>(f)(&args, nullptr);
+      }
+
+      template<typename F, typename TupleType, size_t... Is>
+      static void invoke(void*, F&& f, TupleType& args, std::index_sequence<Is...>)
+      {
+        std::forward<F>(f)(std::forward<std::tuple_element_t<Is, std::remove_reference_t<TupleType>>>(std::get<Is>(args))...);
+      }
+    };
+  }
+
+  class FunctorMap
+  {
+    public:
+      using Fp = std::function<void(void*, void*)>;
+
+      FunctorMap() = default;
+
+      ~FunctorMap()
+      {
+        this->clear();
+      }
+
+      FunctorMap(const FunctorMap&) = delete;
+      FunctorMap& operator= (const FunctorMap&) = delete;
+
+      FunctorMap(FunctorMap&& rhs) noexcept
+        : functors_{std::move(rhs.functors_)}, tidy_{std::move(rhs.tidy_)}
+      {}
+
+      FunctorMap& operator= (FunctorMap&& rhs) noexcept
+      {
+        if (this != &rhs)
+        {
+          this->~FunctorMap();
+          new(this) FunctorMap(std::move(rhs));
+        }
+        return *this;
+      }
+
+      template<typename F>
+      void bind(const std::string& sig, F&& f)
+      {
+        using inspector = meta::Inspector<F>;
+        using arg_t = typename inspector::arg_t;
+        using res_t = typename inspector::res_t;
+        if (functors_.count(sig) <= 0)
+        {
+          tidy_.push_back([sig] {
+            types_<res_t*, arg_t>.erase(sig);
+          });
+        }
+        auto fp = [f = std::forward<F>(f)](void* args, void* res) {
+          using indices = std::make_index_sequence<std::tuple_size<arg_t>::value>;
+          arg_t& arg = *static_cast<arg_t*>(args);
+          res_t* tmp = static_cast<res_t*>(res);
+          meta::call_impl<res_t>::invoke(tmp, f, arg, indices{});
+        };
+        functors_[sig] = std::move(fp);
+        std::lock_guard<std::mutex> lg{ mtx_ };
+        // use res_t* for void type
+        types_<res_t*, arg_t>.insert({ sig, nullptr });
+      }
+
+      template<typename R, typename... Args>
+      R call(const std::string& sig, Args&&... args)
+      {
+        auto params = std::make_tuple(std::forward<Args>(args)...);
+        if(functors_.find(sig) == functors_.end())
+        {
+          return R();
+        }
+        if (types_<R*, std::tuple<Args...>>.count(sig) <= 0)
+        {
+          throw std::runtime_error("parameter mismatch");
+        }
+        auto& fp = functors_[sig];
+        return meta::call_impl<R>::call(fp, params);
+      }
+
+      void clear()
+      {
+        functors_.clear();
+        std::lock_guard<std::mutex> lg{ mtx_ };
+        for (auto& c : tidy_)
+        {
+          c();
+        }
+        tidy_.clear();
+      }
+
+    private:
+      std::map<std::string, Fp> functors_;
+      std::list<std::function<void()>> tidy_;
+      static inline std::mutex mtx_;
+      // vs2017 bug: https://developercommunity.visualstudio.com/content/problem/261624/multiple-initializations-of-inline-static-data-mem.html
+      template<typename R, typename... Args> static inline std::map<std::string, std::tuple<R, Args...>*> types_{};
+  };
+}
+
+#endif
---
+++ fm/test.cpp
@@ -0,0 +1,54 @@
+/***********************************************
+        File Name: test.cpp
+        Author: Abby Cin
+        Mail: abbytsing@gmail.com
+        Created Time: 1/10/19 7:25 PM
+***********************************************/
+
+#include "functor_map.h"
+#include <iostream>
+
+std::string to_str(int x)
+{
+  return std::to_string(x);
+}
+
+struct Foo
+{
+  static std::string foo() { return __func__; }
+  int operator() (int x) { return x * x; }
+};
+
+int main()
+{
+  using std::cout;
+  using std::string;
+  nm::FunctorMap fm;
+
+  // bind
+  fm.bind("lambda", [](int lhs, int rhs) { return lhs + rhs; });
+  fm.bind("lambda2", [](string lhs,  string rhs) { return lhs + rhs; });
+  fm.bind("void_arg_res", [] { std::cout << "void\n"; });
+  fm.bind("void_arg", [] { return "666"; });
+  fm.bind("free_function", to_str);
+  fm.bind("static_member_function", &Foo::foo);
+  Foo foo;
+  fm.bind("capture", [&foo](int x) { return foo(x); });
+
+  fm.call<string>("none exist");
+  cout << fm.call<int>("lambda", 1, 2) << '\n';
+  cout << fm.call<string>("lambda2", string{"hello "}, string{"world"}) << '\n';
+  fm.call<void>("void_arg_res");
+  cout << fm.call<const char*>("void_arg") << '\n';
+  cout << fm.call<string>("free_function", 233) << '\n';
+  cout << fm.call<string>("static_member_function") << '\n';
+  cout << fm.call<int>("capture", 16) << '\n';
+  try
+  {
+    fm.call<int>("capture");
+  }
+  catch(const std::runtime_error& e)
+  {
+    cout << e.what() << '\n';
+  }
+}
